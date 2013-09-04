4fdd6d276b407a974ae3f209b955b21b1754a318
kaienfr <kaienfr@gmail.com>
2013-09-05 00:04:24 +0200

    Update recent games list & remove redundant recent games from ini file

--- Windows/WndMainWindow.cpp
+++ Windows/WndMainWindow.cpp
@@ -1535,6 +1535,11 @@ namespace MainWindow
 			Update();
 			break;
 
+		case WM_USER_RECREATE_RECENTLIST:
+			NativeMessageReceived("clearrecentlist", "");
+			Update();
+			break;
+
 		case WM_MENUSELECT:
 			// Unfortunately, accelerate keys (hotkeys) shares the same enabled/disabled states
 			// with corresponding menu items.
